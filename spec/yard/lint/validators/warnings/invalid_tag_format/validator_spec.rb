774448052501dd55081d28d2749c72355cb55ab9
Maciej Mensfeld <maciej@mensfeld.pl>
2025-11-07 17:38:57 +0100

    remarks

--- spec/yard/lint/validators/warnings/invalid_tag_format/validator_spec.rb
+++ spec/yard/lint/validators/warnings/invalid_tag_format/validator_spec.rb
@@ -14,9 +14,9 @@ RSpec.describe Yard::Lint::Validators::Warnings::InvalidTagFormat::Validator do
   describe '#call' do
     it 'returns a hash with stdout, stderr, exit_code keys' do
       allow(validator).to receive(:shell).and_return({ stdout: '', stderr: '', exit_code: 0 })
-      
+
       result = validator.call
-      
+
       expect(result).to be_a(Hash)
       expect(result).to have_key(:stdout)
       expect(result).to have_key(:stderr)
